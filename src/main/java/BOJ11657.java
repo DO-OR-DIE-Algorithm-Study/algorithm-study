1ddcbc11ccfe382ea48100d971e66e91b54bf44f
LSM <leesung2925@gmail.com>
2025-09-07 16:16:36 +0900

    [feat] week4: 1477

---
+++ src/main/java/BOJ11657.java
@@ -0,0 +1,11 @@
+public class BOJ11657 {
+    public static void main(String[] args) {
+        /**
+         * 먼저 싸이클이 존재하는지, 해당 싸이클의 가중치의 합이 음수인지 체크
+         * 만약 음수라면 -1 출력 후 종료
+         * 아니라면 다익스트라 진행
+         */
+
+
+    }
+}
